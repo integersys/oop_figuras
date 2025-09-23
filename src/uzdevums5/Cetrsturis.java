f4899503737b8cd11f45d11d9753522b838eb70b
Intars Voitkevičs <intars.voitkevics@sk.lvt.lv>
2025-09-23 11:35:19 +0300

    Pabeigta klase Četrstūris

--- src/uzdevums5/Cetrsturis.java
+++ src/uzdevums5/Cetrsturis.java
@@ -1,5 +1,33 @@
 package uzdevums5;
 
 public class Cetrsturis {
-
-}
+	public double platums, augstums;
+	public Centrs punkts;
+	
+	// konstruktoru parslogošana
+	
+	public Cetrsturis() {
+		punkts = new Centrs (0, 0);
+	}
+	
+	public Cetrsturis(Centrs punkts, double p, double a) {
+		this.punkts = punkts;
+		platums = p;
+		augstums = a;
+	}
+	
+		public String izvaditCetrsturaInfo() {
+			return "Platums: "+platums+" augstums: "+augstums+"cm";
+		}
+		
+		/*
+		// uztaisīt metodi, kas aprēķina laukumu un atgriež tikai rezultātu
+		// public double cLaukums(){
+		double s = platums * augstums;
+		return s;
+		}*/
+		
+		public double cetrlaukums(double platums, double augstums) {
+			return platums*augstums;
+		}
+	}
