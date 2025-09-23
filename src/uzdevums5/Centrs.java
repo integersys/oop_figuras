ac8be0822a854213fb291d0ea848d7dbcd2ec18b
Intars Voitkevičs <intars.voitkevics@sk.lvt.lv>
2025-09-23 11:20:55 +0300

    Pabeigta pirmā klase Punkts

---
+++ src/uzdevums5/Centrs.java
@@ -0,0 +1,23 @@
+package uzdevums5;
+
+public class Centrs {
+	//atributi
+	public int x, y;
+	// konstruktors
+	public Centrs (int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	// metodes
+	public String izvaditKoordinatas() {
+			return "Centra punkts atrodas:\n x = "+x+" y = "+y;
+		}
+	
+	public void parvietot (int koordX, int koordY) {
+		x = x + koordX;
+		y = y + koordY;
+	}
+	
+	}
+
+	
